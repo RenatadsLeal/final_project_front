2ba23864c1544585bc1ebd2566a426866b66f050
Renata Leal <85504267+RenatadsLeal@users.noreply.github.com>
2021-12-12 02:16:39 -0300

    "swal add product"
    Co-authored-by: Ailla Freire <aillaf@users.noreply.github.com>
    Co-authored-by: Carolina Hakamada <hakacarol@users.noreply.github.com>
    Co-authored-by: Clarissa Farencena Arruda <ClarissaFA@users.noreply.github.com>
    Co-authored-by: Helena Perdigueiro <helenaperdigueiro@users.noreply.github.com>

--- src/reducers/basketReducer.js
+++ src/reducers/basketReducer.js
@@ -1,11 +1,24 @@
+import Swal from 'sweetalert2';
+
 export const basketReducer = (state, action) => {
     switch (action.type) {
         case 'ADD_PRODUCT':
             const checkIfProductAlreadyExists = state.filter(({ product }) => (
                 product.id === action.payload.product.id
+
             ));
             if (!checkIfProductAlreadyExists.length) {
+                Swal.fire({
+                    text: 'Produto colocado na cesta',
+                    confirmButtonColor: '#000000',
+                    imageUrl: 'https://cdn-icons-png.flaticon.com/512/3014/3014466.png',
+                    imageWidth: 100,
+                    width: 280,
+                    height: 150,
+                    imageAlt: 'bread',
+                })
                 return [...state, action.payload];
+
             }
             return state;
         case 'DELETE_PRODUCT':
